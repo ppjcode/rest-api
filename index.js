d265eb20587facea03f857e0fddcf9bcf7f06645
ppjcode <ppj.code@gmail.com>
2020-07-21 14:16:49 -0500

    add Clients and Products

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
 
 
 // starting app
 const app = express();
+
+// connect to db
+mongoose.connect('mongodb://localhost/restapi', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 
 
@@ -18,6 +25,4 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', routes());
 
 // starting server
-app.listen(5000, () => {
-    console.log(`Server started on port`);
-});
+app.listen(5000, () => console.log(`Server started on port`));
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,49 @@
 const express = require('express');
 const router = express.Router();
 const clientsController = require('../controllers/clientController');
+const productsController = require('../controllers/productsController');
 
 module.exports = () => {
 
+    /** CLIENTS */
+
+    // create client
     router.post('/clients', clientsController.newClient);
+
+    // show all clients
+    router.get('/clients', clientsController.showClients);
+
+    // show one client
+    router.get('/clients/:id', clientsController.showClient);
+
+    // update one client
+    router.put('/clients/:id', clientsController.updateClient);
+
+    // delete one client
+    router.delete('/clients/:id', clientsController.deleteClient);
+
+    /** PRODUCTS */
+
+    // create product
+    router.post('/products',
+        productsController.uploadFile,
+        productsController.createProduct
+    );
+
+    // show all products
+    router.get('/products', productsController.showProducts);
+
+    // show one product
+    router.get('/products/:id', productsController.showProduct);
+
+    // update one product
+    router.put('/products/:id',
+        productsController.uploadFile,
+        productsController.updateProduct
+    );
+
+    // delete one product
+    router.delete('/products/:id', productsController.deleteProduct);
 
     return router;
 };
